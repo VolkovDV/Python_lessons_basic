3f32fe9f833fe9c5f47c4327104192abc8f7527c
Dmitrii <volkovdmvd@gmail.com>
2019-04-10 22:40:17 +0300

    Second lesson DONE

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -4,11 +4,17 @@
 # Округление должно происходить по математическим правилам (0.6 --> 1, 0.4 --> 0).
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
+
 def my_round(number, ndigits):
-    pass
+    number_int = number * (10 ** (ndigits+1))
+    last_digit = number_int % 10
+    if last_digit > 4:
+        number_int = number_int//10 + 1
+    number_int = number_int / (10 ** ndigits)
+    return number_int
 
 
-print(my_round(2.1234567, 5))
+print(my_round(2.2314567, 5))
 print(my_round(2.1999967, 5))
 print(my_round(2.9999967, 5))
 
@@ -20,7 +26,22 @@ print(my_round(2.9999967, 5))
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
 def lucky_ticket(ticket_number):
-    pass
+    len_ticket = len(str(ticket_number))
+    if len_ticket != 6:
+        return False
+    else:
+
+        def sum_digit(digit):
+            sum_d = 0
+            for el in digit:
+                sum_d += int(el)
+            return sum_d
+        first_half = str(ticket_number)[:int(len_ticket/2)]
+        second_half = str(ticket_number)[int(len_ticket/2):]
+        if sum_digit(first_half) == sum_digit(second_half):
+            return True
+        else:
+            return False
 
 
 print(lucky_ticket(123006))
--- lesson03/home_work/hw03_normal.py
+++ lesson03/home_work/hw03_normal.py
@@ -2,8 +2,16 @@
 # Напишите функцию, возвращающую ряд Фибоначчи с n-элемента до m-элемента.
 # Первыми элементами ряда считать цифры 1 1
 
+
 def fibonacci(n, m):
-    pass
+    list_fib = [1, 1]
+    if m <= 1:
+        return list_fib[m]
+    else:
+        for i in range(2, m + 1):
+            list_fib.append(list_fib[i - 1] + list_fib[i - 2])
+        return list_fib[n-1:m]
+
 
 # Задача-2:
 # Напишите функцию, сортирующую принимаемый список по возрастанию.
@@ -12,7 +20,13 @@ def fibonacci(n, m):
 
 
 def sort_to_max(origin_list):
-    pass
+    for _ in origin_list:
+        for i, el in enumerate(origin_list):
+            if i < len(origin_list)-1:
+                if el > origin_list[i+1]:
+                    origin_list[i], origin_list[i+1] = origin_list[i+1], origin_list[i]
+    return origin_list
+
 
 sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
 
@@ -21,7 +35,32 @@ sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
 # Разумеется, внутри нельзя использовать саму функцию filter.
 
 
+def my_filter(func, obj):
+    result = []
+    for el in obj:
+        if func(el):
+            result.append(el)
+    return result
+
+
 # Задача-4:
 # Даны четыре точки А1(х1, у1), А2(x2 ,у2), А3(x3 , у3), А4(х4, у4).
 # Определить, будут ли они вершинами параллелограмма.
+
+import math
+
+
+def parallelogramm(a1, a2, a3, a4):
+    def len_side(point_one, point_two):
+        return math.sqrt((point_two[0]-point_one[0])**2 + (point_two[1] - point_one[1])**2)
+    a = [a1, a2, a3, a4]
+    a.sort()
+    if len_side(a[0], a[1]) == len_side(a[2], a[3]) and len_side(a[0], a[2]) == len_side(a[1], a[3]):
+        return True
+    else:
+        return False
+
+
+A1, A2, A3, A4 = [2, 4], [-3, 7], [-6, 6], [-1, 3]
+print(parallelogramm(A1, A2, A3, A4))
 
